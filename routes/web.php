834d4c5432010b850173d1686736b29123bad2c7
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2021-08-27 15:33:32 +0700

    Add Auth Routes laravel/ui and IsAdmin Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KotaController;
 use App\Http\Controllers\NegaraController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +15,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes(['verify' => true]);
+
 Route::redirect('/','/negara');
 
 Route::get('/negara', [NegaraController::class, 'index'])->name('negara.index');
-Route::get('/negara/create', [NegaraController::class, 'create'])->name('negara.create');
-Route::get('/negara/{negara}/edit', [NegaraController::class, 'edit'])->name('negara.edit');
-Route::put('/negara/{negara}', [NegaraController::class, 'update'])->name('negara.update');
+Route::middleware(['auth','verified','admin'])->group(function(){
+    Route::get('/negara/create', [NegaraController::class, 'create'])->name('negara.create');
+    Route::get('/negara/{negara}/edit', [NegaraController::class, 'edit'])->name('negara.edit');
+    Route::put('/negara/{negara}', [NegaraController::class, 'update'])->name('negara.update');
+    Route::delete('/negara/{negara}', [NegaraController::class, 'destroy'])->name('negara.destroy');
+    Route::post('/negara', [NegaraController::class, 'store'])->name('negara.store');
+});
 Route::get('/negara/{negara}', [NegaraController::class, 'show'])->name('negara.show');
-Route::delete('/negara/{negara}', [NegaraController::class, 'destroy'])->name('negara.destroy');
-Route::post('/negara', [NegaraController::class, 'store'])->name('negara.store');
 
 Route::get('/kota', [KotaController::class, 'index'])->name('kota.index');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
